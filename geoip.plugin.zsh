5cce157de6f8b88d246dfaf268c262d9de960039
Ting Huang <durant.huang@gmail.com>
2017-04-20 18:06:52 +0800

    bash script init

---
+++ geoip.plugin.zsh
@@ -0,0 +1,39 @@
+get_geo_info(){
+	localize
+
+	if [[ $# -eq 0 ]]; then
+		while IFS=, read -r COUNTRY REGION CITY TIMEZONE ISP IP;
+		do
+			print_info
+		done <<< $(wget -qO- http://ip-api.com/csv/\?lang\=$lang_local\&fields\=country,regionName,city,timezone,isp,query);
+	else
+		while IFS=, read -r COUNTRY REGION CITY TIMEZONE ISP IP;
+		do
+			print_info
+		done <<< $(wget -qO- http://ip-api.com/csv/$1\?lang\=$lang_local\&fields\=country,regionName,city,timezone,isp,query);
+	fi
+}
+
+localize(){
+	lang_local='en'
+	name_ip='IP Address'
+	name_location='Location'
+	name_timezone='Timezone'
+	name_isp="ISP"
+	if locale | grep -q zh_CN; then
+		lang_local='zh-CN'
+		name_ip='IP地址'
+		name_location='位置'
+		name_timezone='时区'
+		name_isp="运营商"
+	fi
+}
+
+print_info(){
+	echo "$name_ip: $IP"
+	echo "$name_location: $COUNTRY -> $REGION -> $CITY"
+	echo "$name_timezone: $TIMEZONE"
+	echo "$name_isp: $ISP"
+}
+
+alias geoip=get_geo_info
